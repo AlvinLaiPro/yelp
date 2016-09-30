e03890e458c07dc0c7eea4958cfaf8ce5b60aa57
赖绿林 <123580@nd.com>
2016-09-30 18:27:31 +0800

    react demo first part

---
+++ webpack.config.js
@@ -0,0 +1,88 @@
+const webpack = require('webpack');
+const fs = require('fs');
+const path = require('path'),
+	join = path.join,
+	resolve = path.resolve;
+const getConfig = require('hjs-webpack');
+
+const root = resolve(__dirname);
+const src = join(root, 'src');
+const modules = join(root, 'node_modules');
+const dest = join(root, 'dist');
+
+const NODE_ENV = process.env.NODE_ENV;
+const isDev = NODE_ENV === 'development';
+
+
+var config = getConfig({
+	isDev: isDev,
+	in : join(src, 'app.js'),
+	out: dest,
+	clearBeforeBuild: true
+});
+// console.dir(config);
+
+config.postcss = [].concat([
+	require('precss')({}),
+	require('autoprefixer')({}),
+	require('cssnano')({})
+]);
+
+const cssModulesNames = `${isDev ? '[path][name]__[local]__' : ''}[hash:base64:5]`;
+const matchCssLoaders = /(^|!)(css-loader)($|!)/;
+
+const findLoader = (loaders, match) => {
+	const found = loaders.filter(l => l &&
+		l.loader && l.loader.match(match));
+	return found ? found[0] : null;
+}
+
+const cssloader = findLoader(config.module.loaders, matchCssLoaders);
+console.dir(cssloader)
+const newloader = Object.assign({}, cssloader, {
+	test: /module\.css$/,
+	include: [src],
+	loader: cssloader.loader
+		.replace(matchCssLoaders, `$1$2?modules&localIdentName=${cssModulesNames}$3`)
+});
+
+config.module.loaders.push(newloader);
+
+cssloader.test = new RegExp(`[^module]${cssloader.test.source}`);
+
+cssloader.loader = newloader.loader;
+
+config.module.loaders.push({
+  test: /\.css$/,
+  include: [modules],
+  loader: 'style!css'
+})
+
+const dotenv = require('dotenv');
+console.log(dotenv)
+const dotEnvVars = dotenv.config();
+
+console.log(dotEnvVars)
+const environmentEnv = dotenv.config({
+	path: join(root, 'config', `${NODE_ENV}.config.js`),
+	silent: false,
+});
+
+const envVariables = Object.assign({}, dotEnvVars, environmentEnv);
+
+const defines = Object.keys(envVariables)
+	.reduce((memo, key) => {
+		const val = JSON.stringify(envVariables[key]);
+		memo[`__${key.toUpperCase()}__`] = val;
+		return memo;
+	}, {
+		__NODE_ENV__: JSON.stringify(NODE_ENV)
+	});
+
+config.plugins = [
+	new webpack.DefinePlugin(defines)
+].concat(config.plugins);
+
+config.resolve.root = [src, modules]
+
+module.exports = config;
